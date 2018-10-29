0cbf2befd6e112bba8cfa45b5c02128071398844
Flávio G Souza <masterchieffg2@gmail.com>
2018-10-28 23:44:19 -0300

    Add files via upload

---
+++ src/com/concessionaria/domain/Venda.java
@@ -0,0 +1,85 @@
+<<<<<<< HEAD
+package com.concessionaria.domain;
+
+public class Venda {
+	private int id;
+	private String nome;
+	private String cpf;
+	private Carro placa;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Carro getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(Carro placa) {
+		this.placa = placa;
+	}
+
+}
+=======
+package com.concessionaria.domain;
+
+public class Venda {
+	private int id;
+	private String nome;
+	private String cpf;
+	private Carro placa;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Carro getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(Carro placa) {
+		this.placa = placa;
+	}
+
+}
+>>>>>>> 9fc7300cf5dfc6705b6724bdfb10fa261909b98e
---
+++ src/com/concessionaria/util/JSFUtil.java
@@ -0,0 +1,43 @@
+<<<<<<< HEAD
+package com.concessionaria.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class JSFUtil {
+	public static void adicionarMensagemSucesso(String mensagem){
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, mensagem, mensagem);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msg);
+		
+	}
+	
+	public static void adicionarMensagemErro(String mensagem){
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, mensagem);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msg);
+		
+	}
+}
+=======
+package com.concessionaria.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class JSFUtil {
+	public static void adicionarMensagemSucesso(String mensagem){
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, mensagem, mensagem);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msg);
+		
+	}
+	
+	public static void adicionarMensagemErro(String mensagem){
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, mensagem);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msg);
+		
+	}
+}
+>>>>>>> 9fc7300cf5dfc6705b6724bdfb10fa261909b98e
